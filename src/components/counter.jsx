9f1b05f903c01bbb13265d8c29e551be018f9745
pedro <pedro@ncf.ca>
2020-03-21 19:19:53 -0400

    Deleting the counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -25,7 +25,7 @@ class Counter extends Component {
         </button>
         <button
           className="btn btn-danger btn-sm m-2"
-          onClick={this.props.onDelete}
+          onClick={() => this.props.onDelete(this.props.id)}
         >
           Delete
         </button>
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,8 +10,10 @@ class Counters extends Component {
       { id: 5, value: 10 }
     ]
   };
-  handleDelete = () => {
-    console.log("Evnent Handler called");
+  handleDelete = counterId => {
+    console.log("Evnent Handler called", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters: counters });
   };
   render() {
     return (
@@ -21,7 +23,7 @@ class Counters extends Component {
             key={counter.id}
             onDelete={this.handleDelete}
             value={counter.value}
-            selected={true}
+            id={counter.id}
           />
         ))}
       </div>
