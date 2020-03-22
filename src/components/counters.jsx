efeab02f9a1afb707f418f387ecd5e8b6d9648c8
pedro <pedro@ncf.ca>
2020-03-22 15:10:07 -0400

    added reset function that doesnt work properly

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,6 +10,14 @@ class Counters extends Component {
       { id: 5, value: 10 }
     ]
   };
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0; // this reset function will reset the value of each counter in the counter array
+      return c; // but the dom doesnt get updated because the counter in counter.jsx
+    }); // has property this.props.counter.value which only gets initialized
+    // from this array once
+    this.setState({ counters: counters });
+  };
   handleDelete = counterId => {
     console.log("Evnent Handler called", counterId);
     const counters = this.state.counters.filter(c => c.id !== counterId);
@@ -18,6 +26,12 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button // Added button here to demonstrate a problem, see onclick function
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
