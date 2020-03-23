c2485dfbe18ae91ff383db05c22c49996a98272b
pedro <pedro@ncf.ca>
2020-03-22 20:01:57 -0400

    Lifted Counters' state to App.js, Counters is now controlled component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Counters from "./components/counters";
+import { render } from "@testing-library/react";
+import Navbar from "./components/navbar";
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 3 },
+      { id: 3, value: 6 },
+      { id: 4, value: 8 },
+      { id: 5, value: 10 }
+    ]
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
 
-function App() {
-  return (
-    <React.Fragment>
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-      </nav>
-      <Counters />
-    </React.Fragment>
-  );
+    this.setState({ counters: counters });
+  };
+  handleDelete = counterId => {
+    console.log("Evnent Handler called", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+  handleIncrement = counter => {
+    console.log("Increment handler called", counter);
+    const counters = [...this.state.counters]; // copy of counters array, with original counter objects inside
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter }; // copy of contents of counter object desired
+    counters[index].value++;
+    console.log(this.state.counters[index]);
+    this.setState({ counters: counters }); // overwrite original state object
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <Counters
+          counters={this.state.counters}
+          onReset={this.handleReset}
+          onDelete={this.handleDelete}
+          onIncrement={this.handleIncrement}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,56 +1,24 @@
 import React, { Component } from "react";
 import Counter from "./counter";
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 3 },
-      { id: 3, value: 6 },
-      { id: 4, value: 8 },
-      { id: 5, value: 10 }
-    ]
-  };
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0; // this reset function will reset the value of each counter in the counter array
-      return c; // but the dom doesnt get updated because the counter in counter.jsx
-    }); // has property this.props.counter.value which only gets initialized
-    // from this array once
-    this.setState({ counters: counters });
-  };
-  handleDelete = counterId => {
-    console.log("Evnent Handler called", counterId);
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters: counters });
-  };
-  handleIncrement = counter => {
-    console.log("Increment handler called", counter);
-    const counters = [...this.state.counters]; // copy of counters array, with original counter objects inside
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter }; // copy of contents of counter object desired
-    counters[index].value++;
-    console.log(this.state.counters[index]);
-    this.setState({ counters: counters }); // overwrite original state object
-  };
+  // Counters becomes a controlled component in this step. all references to this.state are removed
   render() {
     return (
       <div>
-        <button // Added button here to demonstrate a problem, see onclick function
-          onClick={this.handleReset}
+        <button
+          onClick={this.props.onReset} //changed function to call parent component (App.js)
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map((
+          counter // changed from this.state.counters - in app.js counters prop is created - muey importante
+        ) => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
-            counter={counter} // passing the whole object as a prop instead of the objects state properties below
-            //value={counter.value}
-            //id={counter.id}
-            // this will require an update in the call from the other object
-            // for instance in counter.jsx change this.props.id to this.props.counter.id
+            onDelete={this.props.onDelete} //changed function to call parent component (App.js)_
+            onIncrement={this.props.onIncrement} // here also
+            counter={counter}
           />
         ))}
       </div>
